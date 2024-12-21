e1444a561de0209d94a8de36db50dc0662238d94
Batuhan Balın <batuhanbalin@hotmail.com>
2024-12-21 17:32:22 +0300

    Work on Comments

---
+++ CoreDemo/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Comment
+{
+    public class CommentListByBlog : ViewComponent
+    {
+        CommentManager cm = new CommentManager(new EfCommentRepo());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetList(8);
+            return View(values);
+        }
+    }
+}
