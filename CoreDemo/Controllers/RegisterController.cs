daf0162cc00ab1df0b74c726b4a91032c653c5b6
Batuhan Balın <batuhanbalin@hotmail.com>
2025-01-13 23:20:11 +0300

    Register Operations

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class RegisterController : Controller
+	{	
+
+		WriterManager wm = new  WriterManager(new EfWriterRepo());
+
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(Writer p)
+		{
+			p.WriterStatus=true;
+			p.WriterAbout = "Deneme Test";
+			wm.WriterAdd(p);
+			return RedirectToAction("Index","Blog");
+		}
+	}
+}
